7f8d789489e413b89c35ebcc6e842af4cc250510
Andrey Ivanov <prioritet2503@gmail.com>
2022-08-03 22:48:15 +0300

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Component } from "react";
 import Searchbar from "./Searchbar";
-import ImageInfo from "./ImageInfo";
 import s from './App.module.css';
 import { ToastContainer } from 'react-toastify';
+import ImageGallery from "./ImageGallery/ImageGallery";
 
 export default class App extends Component {
   state = {
@@ -10,7 +10,6 @@ export default class App extends Component {
   };
   
   handleFormSubmit = imageInfo => {
-    console.log(imageInfo);
     this.setState({ imageInfo });
   };
   
@@ -18,7 +17,7 @@ export default class App extends Component {
     return (
       <div className={s.app}>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageInfo imageInfo={this.state.imageInfo}/>
+        <ImageGallery imageInfo={this.state.imageInfo}/>
         {/* {this.state.loading && <h1>reload...</h1>}
       {this.state.pokemon && <div>Hello</div>} */}
         <ToastContainer position="top-center" theme="colored" />
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,58 @@
 
+import { Component } from "react";
 
-<ul class="gallery">
-  {/* <!-- Набор <li> с изображениями --> */}
-</ul>
+
+
+export default class ImageGallery extends Component {
+    state = {
+      image: null,
+      error: null,
+      status: 'idle',
+}
+  componentDidUpdate(prevProps, prevState) {
+    const prevImage = prevProps.imageInfo;
+    const nextImage = this.props.imageInfo;
+
+    if (prevImage !== nextImage) {
+      this.setState({ status: 'pending'});      
+        fetch(`https://pixabay.com/api/?q=${nextImage}&page=1&key=27913920-68ceae66209fe678afbf6b110&image_type=photo&orientation=horizontal&per_page=12`)
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+            return Promise.reject(
+              new Error(`Нет таких картинок c именем ${nextImage}`),
+            );
+      })
+          .then(image => this.setState({ image, status:'resolved' }))
+          .catch(error=>this.setState({error, status:'rejected'}))            
+        }
+    }
+  render() {
+    const { image, error, status } = this.state;
+    
+    if (status === 'idle') {
+      return <div>Введите имя</div>
+    }
+    if (status === 'pending') {
+      return <div> Загружаем... </div>
+    }
+    if (status==='rejected') {
+      return <h1>{error.message}</h1>
+    }
+    if (status === 'resolved') {
+      return (<>
+      <div>{image.total}</div>
+        <ul className="gallery">
+          {image.hits.map(item => (<li key={item.id}>
+            <img
+              src={item.previewURL}
+              width="240"
+              height="100"
+              alt={item.tags} />
+          </li>))}
+        </ul>
+      </>)
+    }
+  }
+};
